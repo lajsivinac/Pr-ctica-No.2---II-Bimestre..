744f34969d555e7d52232ebb5fee4bc76a6272db
lajsivinac <166849801+lajsivinac@users.noreply.github.com>
2024-04-12 13:00:37 -0600

    Segunda parte
    Escritura de un pin digital.

---
+++ Segunda_parte/Segunda_parte.ino
@@ -0,0 +1,28 @@
+/*
+ Fundacion Kinal
+Centro Educativo Tecnico Laboral Kinal
+Quinto Perito
+Quinto Electronica
+Codigo Tecnico: EB5AM
+Curso: Taller de electronica digital y reparacion de computadoras I
+Proyecto: Escritura en un pin digital (Encender o apagar un pin digital)
+Dev: Ludwyn Steven Ajsivinac Alonzo
+Fecha: 12 de Abril
+*/
+// Directivas de procesador
+#define LedNaranja 4 //Pin4 conectado a un led
+#define tiempo_espera 500 //Tiempo entre estados del led
+
+void setup()
+{
+  pinMode(LedNaranja, OUTPUT); //Pin4 como salida
+  digitalWrite(LedNaranja, LOW);//LED conectado al pin13 inicia apagado
+}
+
+void loop()
+{
+  digitalWrite(LedNaranja, HIGH);// Enciendo el led conectado al  pin4
+  delay(tiempo_espera); //espera de 500 milisegundos
+  digitalWrite(LedNaranja, LOW);//Apago el led conectado al pin4
+  delay(tiempo_espera); //espera de 500 milisegundos
+}
