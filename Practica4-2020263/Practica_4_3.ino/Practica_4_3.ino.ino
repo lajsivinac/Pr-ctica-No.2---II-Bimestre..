5943847f40fcc1f2ce21ac55d57de98ae6ccea20
lajsivinac <166849801+lajsivinac@users.noreply.github.com>
2024-05-06 23:37:31 -0600

    Update Practica_4_3.ino.ino

--- Practica4-2020263/Practica_4_3.ino/Practica_4_3.ino.ino
+++ Practica4-2020263/Practica_4_3.ino/Practica_4_3.ino.ino
@@ -10,133 +10,128 @@
    Fecha: 26 de abril
 */
 
+int secuencia[] = {6, 7, 8, 9, 10}; 
+int salidaA = 11;
+int salidaB = 12;
+int salidaC = 14;
+int salidaD = 15;
+int salidaE = 16;
+int salidaF = 17;
+int salidaG = 18;
 
-int secuencia[] = {2, 3, 4, 5, 6}; 
-int A = 11;
-int B = 12;
-int C = 14;
-int D = 15;
-int E = 16;
-int F = 17;
-int G = 18;
-
-void setup()
-   {
-  
-      pinMode(2, INPUT);
-      pinMode(3, INPUT);
-      pinMode(4, INPUT);
-      pinMode(5, INPUT);
-      pinMode(A, OUTPUT);
-      pinMode(B, OUTPUT);
-      pinMode(C, OUTPUT);
-      pinMode(D, OUTPUT);
-      pinMode(E, OUTPUT);
-      pinMode(F, OUTPUT);
-      pinMode(G, OUTPUT);
-  
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(salidaA, OUTPUT);
+  pinMode(salidaB, OUTPUT);
+  pinMode(salidaC, OUTPUT);
+  pinMode(salidaD, OUTPUT);
+  pinMode(salidaE, OUTPUT);
+  pinMode(salidaF, OUTPUT);
+  pinMode(salidaG, OUTPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  
-
-  if(digitalRead(2)== HIGH && digitalRead(4)== LOW){
+  if (digitalRead(2) == HIGH && digitalRead(4) == LOW) {
     delay(3000);
     Serial.println("Practica No.4 Ludwyn Steven Ajsivinac Alonzo");
   }
-  if(digitalRead(3)==HIGH && digitalRead(5)== LOW ){
-for (int m = 0; m < 1; m++) {
-    for (int led = 0; led < 4; led++) { 
-      digitalWrite(secuencia[led], HIGH);
-      delay(400);
-      digitalWrite(secuencia[led], LOW);
+  
+  if (digitalRead(3) == HIGH && digitalRead(5) == LOW) {
+    for (int m = 0; m < 1; m++) {
+      for (int led = 0; led < 4; led++) { 
+        digitalWrite(secuencia[led], HIGH);
+        delay(400);
+        digitalWrite(secuencia[led], LOW);
+      }
+      for (int led = 4; led >= 0; led--) {
+        digitalWrite(secuencia[led], HIGH);
+        delay(400);
+        digitalWrite(secuencia[led], LOW);
+      }
     }
-
-    for (int led = 4; led >= 0; led--) {
-      digitalWrite(secuencia[led], HIGH);
-      delay(400);
-      digitalWrite(secuencia[led], LOW);
-
-    }
-}
   }
-  if(digitalRead(4)== HIGH && digitalRead(2)==LOW){ 
-    for(int i = 0; i<1 ; i++) {
-      digitalWrite( A, HIGH);
-      digitalWrite( F, HIGH);
-      digitalWrite( E, HIGH);
-      digitalWrite( D, HIGH);
+  
+  if (digitalRead(4) == HIGH && digitalRead(2) == LOW) { 
+    for (int i = 0; i < 1; i++) {
+      digitalWrite(salidaA, HIGH);
+      digitalWrite(salidaF, HIGH);
+      digitalWrite(salidaE, HIGH);
+      digitalWrite(salidaD, HIGH);
       delay(550);
-      digitalWrite( A, LOW);
-      digitalWrite( F, LOW);
-      digitalWrite( E, LOW);
-      digitalWrite( D, LOW);
-      delay (2000);
-      digitalWrite( E, HIGH);
-      digitalWrite( D, HIGH);
-      digitalWrite( C, HIGH);
-      digitalWrite( F, HIGH);
-      digitalWrite( B, HIGH);
-      delay (550);
-      digitalWrite (E, LOW);
-      digitalWrite (D, LOW);
-      digitalWrite (C ,LOW);
-      digitalWrite( F, LOW);
-      digitalWrite( B, LOW);
+      digitalWrite(salidaA, LOW);
+      digitalWrite(salidaF, LOW);
+      digitalWrite(salidaE, LOW);
+      digitalWrite(salidaD, LOW);
       delay(2000);
-      digitalWrite (A, HIGH);
-      digitalWrite (B, HIGH);
-      digitalWrite (C, HIGH);
-      digitalWrite (G, HIGH);
-      digitalWrite (F, HIGH);
-      digitalWrite (E, HIGH);
+      digitalWrite(salidaE, HIGH);
+      digitalWrite(salidaD, HIGH);
+      digitalWrite(salidaC, HIGH);
+      digitalWrite(salidaF, HIGH);
+      digitalWrite(salidaB, HIGH);
       delay(550);
-      digitalWrite (A, LOW);
-      digitalWrite (B, LOW);
-      digitalWrite (C, LOW);
-      digitalWrite (G, LOW);
-      digitalWrite (F, LOW);
-      digitalWrite (E, LOW);
+      digitalWrite(salidaE, LOW);
+      digitalWrite(salidaD, LOW);
+      digitalWrite(salidaC, LOW);
+      digitalWrite(salidaF, LOW);
+      digitalWrite(salidaB, LOW);
       delay(2000);
-      digitalWrite (E, HIGH);
-      digitalWrite (F, HIGH);
-      digitalWrite (A, HIGH);
-      delay (550);
-      digitalWrite (E, LOW);
-      digitalWrite (F, LOW);
-      digitalWrite (A, LOW);
-      delay(2000);
-      digitalWrite (E, HIGH);
-      digitalWrite (F, HIGH);
-      digitalWrite (A, HIGH);
-      digitalWrite (B, HIGH);
-      digitalWrite (G, HIGH);
+      digitalWrite(salidaA, HIGH);
+      digitalWrite(salidaB, HIGH);
+      digitalWrite(salidaC, HIGH);
+      digitalWrite(salidaG, HIGH);
+      digitalWrite(salidaF, HIGH);
+      digitalWrite(salidaE, HIGH);
       delay(550);
-      digitalWrite (E ,LOW);
-      digitalWrite (F, LOW);
-      digitalWrite (A, LOW);
-      digitalWrite (B, LOW);
-      digitalWrite (G, LOW);
+      digitalWrite(salidaA, LOW);
+      digitalWrite(salidaB, LOW);
+      digitalWrite(salidaC, LOW);
+      digitalWrite(salidaG, LOW);
+      digitalWrite(salidaF, LOW);
+      digitalWrite(salidaE, LOW);
       delay(2000);
-      digitalWrite (F, HIGH);
-      digitalWrite (A, HIGH);
-      digitalWrite (B, HIGH);
-      digitalWrite (E, HIGH);
-      digitalWrite (C, HIGH);
-      digitalWrite (D, HIGH);
+      digitalWrite(salidaE, HIGH);
+      digitalWrite(salidaF, HIGH);
+      digitalWrite(salidaA, HIGH);
       delay(550);
-      digitalWrite (F, LOW);
-      digitalWrite (A, LOW);
-      digitalWrite (B, LOW);
-      digitalWrite (E, LOW);
-      digitalWrite (C, LOW);
-      digitalWrite (D, LOW);
+      digitalWrite(salidaE, LOW);
+      digitalWrite(salidaF, LOW);
+      digitalWrite(salidaA, LOW);
+      delay(2000);
+      digitalWrite(salidaE, HIGH);
+      digitalWrite(salidaF, HIGH);
+      digitalWrite(salidaA, HIGH);
+      digitalWrite(salidaB, HIGH);
+      digitalWrite(salidaG, HIGH);
+      delay(550);
+      digitalWrite(salidaE, LOW);
+      digitalWrite(salidaF, LOW);
+      digitalWrite(salidaA, LOW);
+      digitalWrite(salidaB, LOW);
+      digitalWrite(salidaG, LOW);
+      delay(2000);
+      digitalWrite(salidaF, HIGH);
+      digitalWrite(salidaA, HIGH);
+      digitalWrite(salidaB, HIGH);
+      digitalWrite(salidaE, HIGH);
+      digitalWrite(salidaC, HIGH);
+      digitalWrite(salidaD, HIGH);
+      delay(550);
+      digitalWrite(salidaF, LOW);
+      digitalWrite(salidaA, LOW);
+      digitalWrite(salidaB, LOW);
+      digitalWrite(salidaE, LOW);
+      digitalWrite(salidaC, LOW);
+      digitalWrite(salidaD, LOW);
     }
-    }
-  if(digitalRead(4)== HIGH && digitalRead(2)== HIGH ||digitalRead(5)== HIGH && digitalRead(3)== HIGH ){
-      delay(2000);
+  }
+  
+  if ((digitalRead(4) == HIGH && digitalRead(2) == HIGH) || (digitalRead(5) == HIGH && digitalRead(3) == HIGH)) {
+    delay(2000);
     Serial.println("Gracias por su atencion");
   }
 }
