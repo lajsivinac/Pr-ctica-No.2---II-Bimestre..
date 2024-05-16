c283979fd0cecdfdecce7e8281d7d4815f4e3f55
lajsivinac <166849801+lajsivinac@users.noreply.github.com>
2024-05-16 10:24:29 -0600

    Taller 5
    Codigos

---
+++ Practica 5/P5-parte1/P5-parte1/P5-parte1.ino
@@ -0,0 +1,83 @@
+float Kilogramos = 30;
+float resultado1 = resultado1;
+float Amperios = 0.15;
+float resultado2 = resultado2;
+int numero1 [] = {2,6};
+int numero3 [] = {3,2,5,6,7};
+int numero5 [] = {3,4,5,6,7};
+int numero7 [] = {2,3,5,6};
+int numero9 [] = {2,3,4,5,6};
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Conversor");
+  Serial.print("30 Kilogramos = ");
+  resultado1 = conversor_kg_lb(Kilogramos);
+  Serial.print(resultado1);
+  Serial.println(" Libras ");
+
+  Serial.print("0.15 Amperios = ");
+  resultado2 = conversor_A_mA(Amperios);
+  Serial.print(resultado2);
+  Serial.println(" Microamperios");
+}
+
+void loop(){
+ secuencia_de_numeros();
+}
+
+
+float conversor_kg_lb (float kg){
+  float resultado1;
+  resultado1 = Kilogramos * 2.204623;
+  return resultado1;
+}
+
+float conversor_A_mA (float A){
+  float resultado2;
+  resultado2 = Amperios * 1000000;
+  return resultado2;
+}
+
+void pinesdesalida(){
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);   
+}
+
+int secuencia_de_numeros(){
+  for (int u=0; u<2; u++){
+  digitalWrite(numero1[u], HIGH);}
+  delay(1000);
+  for (int u=2; u>=0; u--){
+  digitalWrite(numero1[u],LOW);
+  }
+
+  for (int t =0; t<5; t++){
+  digitalWrite(numero3[t], HIGH);}
+  delay(1000);
+  for (int t =5; t>=0; t--){
+  digitalWrite(numero3[t], LOW);}
+  
+  for (int c =0; c<5; c++){
+  digitalWrite(numero5[c], HIGH);}
+  delay(1000);
+  for (int c =5; c>=0; c--){
+  digitalWrite(numero5[c], LOW);}
+  
+  for (int s =0; s<4; s++){
+  digitalWrite(numero7[s], HIGH);}
+  delay(1000);
+  for (int s =4; s>=0; s--){
+  digitalWrite(numero7[s], LOW);}
+
+  for (int i =0; i<5; i++){
+  digitalWrite(numero9[i], HIGH);}
+  delay(1000);
+  for (int i =5; i>=0; i--){
+  digitalWrite(numero9[i], LOW);}    
+}
+
---
+++ Practica 5/P5-parte2/P5-parte2.ino
@@ -0,0 +1,175 @@
+const int buzzer = 2;
+float Do       = 261.626;
+float Do_Reb   = 277.183;
+float Re       = 393.665;
+float Re_Mib   = 311.127;
+float Mi       = 329.628;
+float Fa       = 349.228;
+float Fa_Solb  = 369.994;
+float Sol      = 391.995;
+float Sol_Lab  = 415.305;
+float La       = 440.000;
+float La_Sib   = 466.164;
+float Si       = 493.883;
+
+void setup ()
+{
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(500);
+  tone(buzzer,La_Sib,466.164);
+  delay(250);
+  tone(buzzer,Sol,391.995);
+  delay(550);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(400);
+  tone(buzzer,La_Sib,466.164);
+  delay(250);
+  tone(buzzer,Sol,391.995);
+  delay(550);
+  
+  tone(buzzer,Re,293.665);
+  delay(500);
+  tone(buzzer,Re,293.665);
+  delay(500);
+  tone(buzzer,Re,293.665);
+  delay(550);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(400);
+  tone(buzzer,La_Sib,466.164);
+  delay(225);
+  tone(buzzer,Fa_Solb,369.994);
+  delay(550);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(400);
+  tone(buzzer,La_Sib,466.164);
+  delay(255);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  tone(buzzer,Sol,391.995);
+  delay(400);
+  
+  tone(buzzer,Fa_Solb,369.994);
+  delay(500);
+  tone(buzzer,Fa,349.228);
+  delay(350);
+  tone(buzzer,Mi,329.628);
+  delay(350);
+  tone(buzzer,Re_Mib,311.127);
+  delay(300);
+  tone(buzzer,Mi,329.628);
+  delay(350);
+  
+  tone(buzzer,Sol_Lab,415.305);
+  delay(300);
+  tone(buzzer,Do_Reb,277.183);
+  delay(500);
+  
+  tone(buzzer,Do,261.626);
+  delay(500);
+  tone(buzzer,Si,493.883);
+  delay(500);
+  tone(buzzer,La_Sib,466.164);
+  delay(500);
+  tone(buzzer,La,440.000);
+  delay(500);
+  tone(buzzer,La_Sib,466.164);
+  delay(500);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(500);
+  tone(buzzer,Fa_Solb,369.994);
+  delay(500);
+  tone(buzzer,Re_Mib,311.127);
+  delay(500);
+  tone(buzzer,Fa_Solb,369.994);
+  delay(500);
+  
+  tone(buzzer,La_Sib,466.164);
+  delay(500);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  tone(buzzer,La_Sib,466.164);
+  delay(500);
+  tone(buzzer,Re,311.127);
+  delay(500);
+  
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  tone(buzzer,Sol,391.995);
+  delay(400);
+  
+  tone(buzzer,Fa_Solb,369.994);
+  delay(500);
+  tone(buzzer,Fa,349.228);
+  delay(350);
+  tone(buzzer,Mi,329.628);
+  delay(350);
+  tone(buzzer,Re_Mib,311.127);
+  delay(300);
+  tone(buzzer,Mi,329.628);
+  delay(350);
+  
+  tone(buzzer,Sol_Lab,415.305);
+  delay(300);
+  tone(buzzer,Do_Reb,277.183);
+  delay(500);
+  
+  tone(buzzer,Do,261.626);
+  delay(500);
+  tone(buzzer,Si,493.883);
+  delay(500);
+  tone(buzzer,La_Sib,466.164);
+  delay(500);
+  tone(buzzer,La,440.000);
+  delay(500);
+  tone(buzzer,La_Sib,466.164);
+  delay(500);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(500);
+  tone(buzzer,Fa_Solb,369.994);
+  delay(500);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(400);
+  tone(buzzer,La_Sib,466.164);
+  delay(200);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  
+  tone(buzzer,Re_Mib,311.127);
+  delay(400);
+  tone(buzzer,La_Sib,466.164);
+  delay(200);
+  tone(buzzer,Sol,391.995);
+  delay(500);
+  
+         
+  
+}
+void loop(){
+
+}
